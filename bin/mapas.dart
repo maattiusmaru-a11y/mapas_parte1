f73c11dccb3b2091dfd06164f8b0656ae00293e0
mattius <jaammattiusmarulanda@gmail.com>
2026-06-09 09:15:00 -0500

    Se agregaron nuevos archivos

---
+++ bin/mapas.dart
@@ -0,0 +1,11 @@
+void main(List<String> arguments) {
+  Map<String, String> agenda = {
+    "laura": "1231234",
+    "sara": "13211111",
+    "salome": "3322244",
+  };
+
+  print("${agenda["laura"]}");
+  print("${agenda["sara"]}");
+  print("${agenda["salome"]}");
+}
